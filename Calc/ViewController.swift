cdf47628880564f74d266ee33825d0d40e0b43cc
Filipp Krupnov <philkrupnov@gmail.com>
2020-10-28 16:42:41 +0600

    Initial Commit

---
+++ Calc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calc
+//
+//  Created by Filipp Krupnov on 10/28/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
