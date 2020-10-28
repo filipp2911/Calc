bd619ad932a15be011be3618b1a7524dfe184deb
Filipp Krupnov <philkrupnov@gmail.com>
2020-10-28 16:50:25 +0600

    Commit

--- Calc/ViewController.swift
+++ Calc/ViewController.swift
@@ -1,19 +1,57 @@
 //
 //  ViewController.swift
-//  Calc
+//  Calculator
 //
-//  Created by Filipp Krupnov on 10/28/20.
+//  Created by Filipp Krupnov on 10/9/20.
 //
 
 import UIKit
-
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var display: UILabel!
+    var userIsInTheMiddleOfTyping = false
+    
+    @IBAction func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        
+        if userIsInTheMiddleOfTyping{
+            let textCurrentlyInDisplay = display!.text!
+            display.text = textCurrentlyInDisplay + digit
+        } else {
+            userIsInTheMiddleOfTyping = true
+            display.text = digit
+        }
     }
-
-
+    
+    var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
+        }
+    }
+    
+    private var idea = CalcWork()
+    
+    @IBAction func performOperation(_ sender: UIButton) {
+        if userIsInTheMiddleOfTyping {
+            idea.setOperand(displayValue)
+            userIsInTheMiddleOfTyping = false
+        }
+        if let mathematicalSymbol = sender.currentTitle {
+            idea.performOperation(mathematicalSymbol)
+        }
+        if let result = idea.result {
+            displayValue = result
+        }
+    }
+    
+    @IBAction func returnToZero(_ sender: UIButton) {
+        userIsInTheMiddleOfTyping = false
+        display.text = "0"
+    }
 }
+
+
 
